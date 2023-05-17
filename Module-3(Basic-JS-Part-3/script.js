f9f50473c80324fb9ec1eb06c4ceeaeb0ff5c2d9
amena21 <amena21@users.noreply.github.com>
2023-05-17 18:08:00 +0600

    Add files via upload

---
+++ Module-3(Basic-JS-Part-3/script.js
@@ -0,0 +1,18 @@
+var x = 5;
+var y = 2;
+var z = x * y;
+var z1 = x + y;
+var z2= x - y;
+var z3 = x % y;
+var z4 = x / y;
+
+
+
+console.log(z);
+console.log(z1);
+console.log(z2);
+console.log(z3);
+console.log(z4);
+
+console.log(x++);
+console.log(++y);
